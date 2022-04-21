116fa30c09fee7a0dc9f2dff227125001d7d61d8
Behzad <saeedibehzad@gmail.com>
2022-04-21 16:00:31 +0430

    complete menu-list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,23 @@ import menu from "../assets/icons/Menu.png";
 import shoppingBag from "../assets/icons/shoppingBag.png";
 import search from "../assets/icons/Search.png";
 import close from "../assets/icons/Close.png";
+import down from "../assets/icons/Down.png";
+import location from "../assets/icons/Location.png";
+import call from "../assets/icons/Call.png";
+import twitter from "../assets/icons/Twitter.png";
+import youtube from "../assets/icons/YouTube.png";
+import instagram from "../assets/icons/Instagram.png";
 const Header = () => {
   const openCloseMenu = () => {
     document.querySelector(".menu-list").classList.toggle("show");
   };
-
+  const dropDown = (e) => {
+    if (e.target.classList.value !== "item-container") {
+      let element = e.target;
+      element.parentElement.childNodes[2].classList.toggle("show");
+      element.parentElement.childNodes[1].classList.toggle("rotate");
+    }
+  };
   return (
     <div className="navbar-container">
       <div className="navbar">
@@ -35,6 +47,122 @@ const Header = () => {
               <h3 className="title">MAN</h3>
               <h3 className="title">KIDS</h3>
             </div>
+            <div className="underline">
+              <div className="selected">
+                <span></span>
+              </div>
+            </div>
+          </div>
+          <div className="menu-list-items">
+            <div
+              className="item-container"
+              id="new"
+              onClick={(e) => dropDown(e)}
+            >
+              <h2>New</h2>
+              <img src={down} alt="" className="arrowDown" />
+              <ul className="item-list">
+                <li>Outer</li>
+                <li>Dress</li>
+                <li>Shirt</li>
+              </ul>
+            </div>
+            <div
+              className="item-container"
+              id="apparel"
+              onClick={(e) => dropDown(e)}
+            >
+              <h2>Apparel</h2>
+              <img src={down} alt="" className="arrowDown" />
+              <ul className="item-list">
+                <li>Outer</li>
+                <li>Dress</li>
+                <li>Blouse/Shirt</li>
+                <li>T-Shirt</li>
+                <li>Knitwear</li>
+                <li>Pants</li>
+                <li>Denim</li>
+                <li>Kids</li>
+              </ul>
+            </div>
+            <div
+              className="item-container"
+              id="bag"
+              onClick={(e) => dropDown(e)}
+            >
+              <h2>Bag</h2>
+              <img src={down} alt="" className="arrowDown" />
+              <ul className="item-list">
+                <li>Outer</li>
+                <li>Dress</li>
+                <li>Shirt</li>
+              </ul>
+            </div>
+            <div
+              className="item-container"
+              id="shoes"
+              onClick={(e) => dropDown(e)}
+            >
+              <h2>Shoes</h2>
+              <img src={down} alt="" className="arrowDown" />
+              <ul className="item-list">
+                <li>Outer</li>
+                <li>Dress</li>
+                <li>Shirt</li>
+              </ul>
+            </div>
+            <div
+              className="item-container"
+              id="beauty"
+              onClick={(e) => dropDown(e)}
+            >
+              <h2>Beauty</h2>
+              <img src={down} alt="" className="arrowDown" />
+              <ul className="item-list">
+                <li>Outer</li>
+                <li>Dress</li>
+                <li>Shirt</li>
+              </ul>
+            </div>
+            <div
+              className="item-container"
+              id="accessories"
+              onClick={(e) => dropDown(e)}
+            >
+              <h2>Accessories</h2>
+              <img src={down} alt="" className="arrowDown" />
+              <ul className="item-list">
+                <li>Outer</li>
+                <li>Dress</li>
+                <li>Shirt</li>
+              </ul>
+            </div>
+          </div>
+          <div className="address-container">
+            <div className="call">
+              <img src={call} alt="" />
+              <h2>(766) 713-8616</h2>
+            </div>
+            <div className="location">
+              <img src={location} alt="" />
+              <h2>Store locator</h2>
+            </div>
+          </div>
+          <div className="underline2">
+            <div className="selected2">
+              <span></span>
+            </div>
+          </div>
+          <div className="social">
+            <a>
+              <img src={twitter} alt="" />
+            </a>
+            <a>
+              <img src={instagram} alt="" />
+            </a>
+            <a>
+              <img src={youtube} alt="" />
+            </a>
           </div>
         </div>
       </div>
