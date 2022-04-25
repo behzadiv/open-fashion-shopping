0e76e7baaa88918f8a13882d848de6ffb9e46bbd
Behzad <saeedibehzad@gmail.com>
2022-04-25 11:48:34 +0430

    develope brands section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,17 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 import Badge from "../common/Badge";
 import { getAllProductsData } from "./feature/allProducts/allProductSlice";
 import "./HomePage.css";
-
+import Prada from "../assets/brands/Prada.png";
+import Burberry from "../assets/brands/Burberry.png";
+import Catier from "../assets/brands/Catier.png";
+import Gucci from "../assets/brands/Gucci.png";
+import Boss from "../assets/brands/Boss.png";
+import Tiffany from "../assets/brands/Tiffany.png";
 const HomePage = () => {
-  const {data,loading,error} = useSelector(state=>state.allProducts)
-  const dispatch = useDispatch()
+  const { data, loading, error } = useSelector((state) => state.allProducts);
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getAllProductsData())
+    dispatch(getAllProductsData());
   }, []);
   console.log(data[(0, 3)]);
   const selectCategory = () => {};
-  if(loading)return <p>loading...</p>
-  if(error)return <p>rejected</p>
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>rejected</p>;
   return (
     <div className="homepage">
       <h2 className="homepage-title">NEW ARRIVAL</h2>
@@ -36,7 +41,7 @@ const HomePage = () => {
         </h3>
       </div>
       <div className="productList">
-        {data.slice(0,4).map((product) => (
+        {data.slice(0, 4).map((product) => (
           <div className="container">
             <div className="card">
               <img src={product.image} alt="" />
@@ -48,6 +53,32 @@ const HomePage = () => {
           </div>
         ))}
       </div>
+      <h1>
+        <a href="">Explore More</a>
+      </h1>
+      <Badge />
+      <section className="brands container">
+        <span>
+          <img src={Prada} alt="" />
+        </span>
+        <span>
+          <img src={Burberry} alt="" />
+        </span>
+        <span>
+          <img src={Boss} alt="" />
+        </span>
+        <span>
+          {" "}
+          <img src={Catier} alt="" />
+        </span>
+        <span>
+          <img src={Gucci} alt="" />
+        </span>
+        <span>
+          <img src={Tiffany} alt="" />
+        </span>
+      </section>
+      <Badge />
     </div>
   );
 };
