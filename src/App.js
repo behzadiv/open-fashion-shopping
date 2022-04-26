d8274219f58d329ec54fedc9a7be325ee9ca42c1
Behzad <saeedibehzad@gmail.com>
2022-04-26 08:38:15 +0430

    develope Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import { store } from "../src/components/feature/store";
+import Footer from "./components/Footer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Header />
         <HomePage />
+        <Footer/>
       </div>
     </Provider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import Badge from "../common/Badge";
+import "./Footer.css"
+import twitter from "../assets/icons/Twitter.png";
+import youtube from "../assets/icons/YouTube.png";
+import instagram from "../assets/icons/Instagram.png";
+
+const Footer = () => {
+    return ( 
+        <section className="footer-container">
+            <div className="footer-social">
+            <a>
+              <img src={twitter} alt="" />
+            </a>
+            <a>
+              <img src={instagram} alt="" />
+            </a>
+            <a>
+              <img src={youtube} alt="" />
+            </a>
+          </div>
+          <Badge/>
+          <div className="footer-address">
+              <h2>support@openui.design</h2>
+              <h2>+60 825 876</h2>
+              <h2>08:00 - 22:00 - Everyday</h2>
+          </div>
+            <Badge/>
+          <div className="footer-links">
+              <h2><a href="">About</a></h2>
+              <h2><a href="">Contact</a></h2>
+              <h2><a href="">Blog</a></h2>
+          </div>
+          <div className="footer-copyright"><p>Copyright OpenUl All Rights Reserved.</p></div>
+        </section>
+     );
+}
+ 
+export default Footer;
