00e4467fb8f2584af5028ab0f36a21b3056344c5
Behzad <saeedibehzad@gmail.com>
2022-04-26 09:45:04 +0430

    create Layout

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import { store } from "../src/components/feature/store";
 import Footer from "./components/Footer";
+import Layout from "./layout/Layout";
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Header />
-        <HomePage />
-        <Footer/>
-      </div>
+      <Layout>
+        <div className="App">
+          <HomePage />
+        </div>
+      </Layout>
     </Provider>
   );
 }
