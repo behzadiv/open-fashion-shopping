806e10d20ebf925404cc69189a723ce5f6b4a75d
Behzad <saeedibehzad@gmail.com>
2022-04-27 11:50:53 +0430

    functionality of Explore more button

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,27 +1,43 @@
 import "./ProductList.css";
-import Product from "../product/Product"
-const ProductList = ({data}) => {
-  
+import Product from "../product/Product";
+import { useState } from "react";
+import foewardArrow from "../../assets/icons/ForwardArrow.png"
+const ProductList = ({ data }) => {
+  const [moreProduct, setMoreProduct] = useState(false);
   const renderProductList = () => {
     return (
       <div className="productList">
-        {data.map((product) => (
-          <Product
-            key={product.id}
-            productImage={product.image}
-            productTitle={product.title}
-            productPrice={product.price}
-          />
-        ))}
+        {!moreProduct
+          ? data
+              .slice(0, 4)
+              .map((product) => (
+                <Product
+                  key={product.id}
+                  productImage={product.image}
+                  productTitle={product.title}
+                  productPrice={product.price}
+                />
+              ))
+          : data.map((product) => (
+              <Product
+                key={product.id}
+                productImage={product.image}
+                productTitle={product.title}
+                productPrice={product.price}
+              />
+            ))}
       </div>
     );
   };
   return (
     <section>
       {renderProductList()}
-      <h1 className="explore-more">
-          <a>Explore More</a>
-        </h1>
+      <div className={!moreProduct? "show show-more":"show show-less"}  onClick={() => setMoreProduct(!moreProduct ? true : false)}>
+        <h2>
+          {!moreProduct ? "Explore More" : "Less"}
+        </h2>
+        <span><img src={foewardArrow} alt="" /></span>
+      </div>
     </section>
   );
 };
