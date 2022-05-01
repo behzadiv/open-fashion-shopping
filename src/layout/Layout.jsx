69b7672a140974b26800076772a6500a38168ece
Behzad <saeedibehzad@gmail.com>
2022-05-01 13:05:06 +0430

    define which page does not need header and footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-const Layout = ({children}) => {
-    return (
-        <>
-        <Header/>
-        {children}
-        <Footer/>
-        </>
-      );
-}
- 
+const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+  //define which page does not need header and footer
+  const isCheckoutRendering = pathname === "/checkout"||"carts";
+  const isCartsRendering = pathname==="/carts"
+  return (
+    <>
+      {!isCartsRendering && <Header />}
+      {children}
+      {!isCheckoutRendering && <Footer />}
+    </>
+  );
+};
+
 export default Layout;
