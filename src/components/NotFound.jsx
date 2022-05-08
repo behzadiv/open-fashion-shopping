ec2417bea10cb8dbc42ad51e1edac884b522e0ee
Behzad <saeedibehzad@gmail.com>
2022-05-08 16:17:20 +0430

    develope NotFound page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import notFound from "../assets/notFound.png";
+import backwardArrow from "../assets/icons/BackwardArrow.png";
+import "./NotFound.css";
+const NotFound = () => {
+  return (
+    <section className="notFound-container container">
+      <h1>PAGE NOT FOUND</h1>
+
+      <img src={notFound} alt="" />
+      <p className="notFound-text">
+        We can't find the page you looking for, it will return to the
+      </p>
+      <NavLink to={"/"} className="notFound-btn">
+        <span>
+          <img src={backwardArrow} alt="" />
+        </span>
+        <span>HOME PAGE</span>
+      </NavLink>
+    </section>
+  );
+};
+
+export default NotFound;
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import AboutUs from "./components/about-us/AboutUs";
+import NotFound from "./components/NotFound";
 import Blog from "./components/blog/Blog";
 import Carts from "./components/carts/Carts";
 import Checkout from "./components/checkout/Checkout";
@@ -12,5 +13,6 @@ const routes = [
     {path:"/blogs" , element:<Blog/>},
     {path:"/contact-us" , element:<ContactUs/>},
     {path:"/about-us" , element:<AboutUs/>},
+    {path:"*",element:<NotFound/>},
 ]
 export default routes
