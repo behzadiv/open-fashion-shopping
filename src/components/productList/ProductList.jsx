12866febe28b3c4675ffdf96d945b6da1d12b970
Behzad <saeedibehzad@gmail.com>
2022-05-08 10:11:29 +0430

    scroll smooth to top when clicked to less btn

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,19 +1,19 @@
 import "./ProductList.css";
 import Product from "../product/Product";
 import { useState } from "react";
-import foewardArrow from "../../assets/icons/ForwardArrow.png"
+import foewardArrow from "../../assets/icons/ForwardArrow.png";
 import { useDispatch } from "react-redux";
 import { addToCarts } from "../feature/cart/cartsSlice";
 const ProductList = ({ data }) => {
   const [moreProduct, setMoreProduct] = useState(false);
-  const dispatch = useDispatch()
-  const addProduct = (id)=>{
+  const dispatch = useDispatch();
+  const addProduct = (id) => {
     console.log(id);
-    let selectedId= Number(id)
-    const myProduct =data.filter((product)=>product.id === selectedId)
+    let selectedId = Number(id);
+    const myProduct = data.filter((product) => product.id === selectedId);
     //pass as object
-    dispatch(addToCarts(myProduct[0]))
-  }
+    dispatch(addToCarts(myProduct[0]));
+  };
   const renderProductList = () => {
     return (
       <div className="productList">
@@ -26,8 +26,8 @@ const ProductList = ({ data }) => {
                   productImage={product.image}
                   productTitle={product.title}
                   productPrice={product.price}
-                  productId = {product.id}
-                 addProduct={()=>addProduct(product.id)}
+                  productId={product.id}
+                  addProduct={() => addProduct(product.id)}
                 />
               ))
           : data.map((product) => (
@@ -36,21 +36,36 @@ const ProductList = ({ data }) => {
                 productImage={product.image}
                 productTitle={product.title}
                 productPrice={product.price}
-                productId = {product.id}
-                addProduct={()=>addProduct(product.id)}
+                productId={product.id}
+                addProduct={() => addProduct(product.id)}
               />
             ))}
       </div>
     );
   };
+  console.log(window.scrollY);
   return (
     <section>
       {renderProductList()}
-      <div className={!moreProduct? "showProduct showProduct-more":"showProduct showProduct-less"}  onClick={() => setMoreProduct(!moreProduct ? true : false)}>
-        <h2>
-          {!moreProduct ? "Explore More" : "Less"}
-        </h2>
-        <span><img src={foewardArrow} alt="" /></span>
+      <div
+        className={
+          !moreProduct
+            ? "showProduct showProduct-more"
+            : "showProduct showProduct-less"
+        }
+        onClick={() => {
+          if (!moreProduct) {
+            setMoreProduct(true);
+          } else {
+            setMoreProduct(false);
+            window.scroll({ top: 1132, behavior: "smooth" });
+          }
+        }}
+      >
+        <h2>{!moreProduct ? "Explore More" : "Less"}</h2>
+        <span>
+          <img src={foewardArrow} alt="" />
+        </span>
       </div>
     </section>
   );
